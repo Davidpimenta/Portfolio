654b4f2d39126dce9516c75c3c2534440017e187
David Sousa Pimenta <130236892+Davidpimenta@users.noreply.github.com>
2024-01-22 18:32:47 -0300

    v4

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
+//Animação de carregamento
+
+//Entrada do site animação
+
+//Entrada do site animação
+
+
+//Animação de carregamento
+
+//Animação typing
+
 var typing = document.getElementById('typing')
 var array = ['David Pimenta', 'Estudante', 'Desenvolvedor Web']
 var cont = 1
-
-/*setTimeout(() => {  
-    typing.style.left = "100%"
-    typing.style.marginLeft = "35px"
-    typing.style.animation = 'piscando 1.5s steps(100000) 1'
-    typing.style.animationFillMode = "forwards";
-}, 1500)
-
-setInterval(() => {
-    typing.style.marginLeft = "0px"
-    typing.style.left = "100%"
-    typing.style.animation = 'typingrevers 1.5s steps(30) 1'
-    typing.style.animationFillMode = "forwards";
-}, 3000)
-
-
-setInterval(() => {
-    if(cont == 3){
-        cont = 0
-    }
-    let textoanimado = document.getElementById('textoanimado')
-    textoanimado.innerText = array[cont]
-    cont++
-    typing.style.left = "0%";
-    typing.style.animation = 'typing 1.5s steps(30) 1'
-    typing.style.animationFillMode = "forwards";
-    setTimeout(() => {
-        typing.style.left = "100%"
-        typing.style.marginLeft = "35px"
-        typing.style.animation = 'piscando 1.5s steps(100000) 1'
-        typing.style.animationFillMode = "forwards";
-    }, 1500)
-}, 6000)
-*/
 
 var veriftyping = 1
 
@@ -68,8 +45,10 @@ typing.addEventListener('animationend', () => {
 
 })
 
+//Animação Typing
 
 
+// Animação Arrow
 var verifarrow = true
 
 var arrow = document.getElementById('arrow')
@@ -84,34 +63,9 @@ arrow.addEventListener('animationend', () => {
     }
 })
 
+//Animação Arrow
 
-var trocafoto = document.querySelector('.conteiner-foto-sobre')
-var veriftrocafoto = false
-trocafoto.addEventListener('click', () => {
-    console.log('click')
-    let foto1 = document.getElementById('foto')
-    let foto2 = document.getElementById('contorno-foto2')
-   
-
-    if(!veriftrocafoto){
-        console.log('click')
-        foto2.style.animation = 'trocafotoa 1s steps(60) 1'
-        foto2.style.animationFillMode = 'forwards'
-        foto1.style.animation = 'trocafotob 1s steps(60) 1'
-        foto1.style.animationFillMode = 'forwards'
-        
-        veriftrocafoto = true
-    } else {
-        foto1.style.animation = 'trocafotoa 1s steps(60) 1'
-        foto1.style.animationFillMode = 'forwards'
-        foto2.style.animation = 'trocafotob 1s steps(60) 1'
-        foto2.style.animationFillMode = 'forwards'
-        veriftrocafoto = false
-    }
-
-})
-
-
+//Animação Scroll
 const menuopt = document.querySelectorAll('.menu-opt')
 menuopt.forEach((item) => {
     item.addEventListener('click', (event) => {
@@ -123,8 +77,6 @@ menuopt.forEach((item) => {
         
         let scrollPositionA = window.scrollY;
         let scrollPositionB = scrollPositionA
-
-
 
         let scrollanimado = setInterval(() => {
             
@@ -151,6 +103,7 @@ menuopt.forEach((item) => {
         
     });
 });
+//Animação Scroll
 
 
 //Menu-Animation
@@ -209,46 +162,69 @@ menuopt.forEach((item) => {
 
 //Menu-Animation
 
+//Animation Skill hover
 
-//Estudos-Carrocel
+var skills = document.querySelectorAll('.skills');
 
-var arrowleft = document.getElementById('arrow-left')
-var arrowright = document.getElementById('arrow-right')
-var contMargin = 0 
+skills.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+        var bg = item.querySelector('.before');
+        bg.classList.add('beforeactive');
+        var text = document.querySelector('.desc')
+        
+        if(item.id == 'htmlconteiner'){
+            text.innerHTML = 'Possuo experiência no uso do <strong class="html">HTML</strong> como linguagem de marcação para o desenvolvimento de páginas web. Tenho familiaridade com suas estruturas fundamentais, assim como compreensão do Document Object Model (DOM).<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
 
-arrowleft.addEventListener('click', () => {
+        if(item.id == 'cssconteiner'){
+            text.innerHTML = 'Tenho experiência na utilização do <strong class="css">CSS</strong> como linguagem de estilização para páginas web. Possuo conhecimento em estilizar elementos CSS, construir estruturas de design eficazes e criar animações para aprimorar a experiência do usuário.<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
 
-    let card = document.getElementById('htmlcsscard')
+        if(item.id == 'jsconteiner'){
+            text.innerHTML = 'Desenvolvi habilidades em <strong class="js">JavaScript</strong>, utilizando-o para criar animações envolventes, manipular dinamicamente o Document Object Model (DOM) e construir funcionalidades interativas que agregam valor à experiência do usuário em páginas web.<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
 
-    if(contMargin == 0){
-        contMargin = -1680
-        card.style.marginLeft = '-1680px'
-    } else {
-        let card = document.getElementById('htmlcsscard')
-        contMargin+=420
-        console.log(contMargin)
-        card.style.marginLeft = contMargin + 'px'
-    }
+        if(item.id == 'jqueryconteiner'){
+            text.innerHTML = 'Aprofundei meu conhecimento ao aprender a utilizar o <strong class="jquery">Jquery</strong>, aprimorando minhas habilidades em efetuar requisições AJAX de forma mais eficiente. Além disso, adquiri expertise na construção de animações envolventes e na configuração facilitada do Document Object Model (DOM), graças a este framework. <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
 
-})
+        if(item.id == 'phpconteiner'){
+            text.innerHTML = 'Adquiri proficiência em <strong class="php">PHP</strong>, o que me possibilitou desenvolver páginas HTML dinâmicas e estabelecer conexões eficientes com o servidor. Agora, posso criar conteúdo interativo, manipular dados dinamicamente e aprimorar a experiência do usuário em minhas aplicações web <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
 
-arrowright.addEventListener('click', () => {
-    
-    let card = document.getElementById('htmlcsscard')
+        if(item.id == 'sqlconteiner'){
+            text.innerHTML = 'Adquiri conhecimentos em <strong class="sql">SQL</strong>, o que me habilitou a armazenar dados dos usuários em aplicações em que eu desempenhava um papel mais focado no back-end. Agora, posso gerenciar eficientemente informações essenciais, proporcionando uma base sólida para o desenvolvimento de aplicações robustas <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
 
-    if(contMargin == -1680){
-        contMargin = 0
-        card.style.marginLeft = '0px'
-    } else {
-        console.log(arrowleft)
-        console.log(arrowright)
-        let card = document.getElementById('htmlcsscard')
-        contMargin-=420
-        console.log(contMargin)
-        card.style.marginLeft = contMargin + 'px'
-    }
-})
+        if(item.id == 'sassconteiner'){
+            text.innerHTML = 'Atualmente, estou dedicando tempo aos estudos de <strong class="sass">SASS</strong>, utilizando esta linguagem de extensão do CSS no desenvolvimento de sites. Busco aprofundar meu conhecimento nesta poderosa ferramenta, explorando suas funcionalidades para otimizar a criação de estilos e aprimorar a qualidade dos meus projetos web. <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
+
+        if(item.id == 'bootsconteiner'){
+            text.innerHTML = 'Aprofundei meus conhecimentos em desenvolvimento web ao aprender <strong class="bootstrap">Bootstrap</strong>. Esta experiência enriqueceu minha capacidade de estruturar elementos HTML e aplicar estilos CSS de maneira mais eficiente, utilizando o framework. Além disso, adquiri habilidades para incorporar facilmente funcionalidades adicionais às páginas, tornando o processo de construção mais ágil e eficaz.<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+            text.style.animation = 'descanimadaA 0.4s steps(60) 1'
+        }
+
+    });
+
+    item.addEventListener('mouseleave', () => {
+        var bg = item.querySelector('.before');
+        bg.classList.remove('beforeactive');
+        var text = document.querySelector('.desc')
+        text.innerHTML = 'As habilidades que adquiri enquanto estudava.<strong class="mousehoveraviso">(Passe o mouse em cima para ver as descrições)</strong>'
+        text.style.animation = 'descanimadaB 0.4s steps(60) 1'
+
+        
+    });
+});
 
 
-//Estudos-Carrocel
 
+//Animation Skill hover
