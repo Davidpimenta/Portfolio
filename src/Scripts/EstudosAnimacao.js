a24f0cc9fc41cac258c918b57b68c5f2012cdd91
David Sousa Pimenta <130236892+Davidpimenta@users.noreply.github.com>
2024-02-02 14:39:45 -0300

    V4Responsive

--- src/Scripts/EstudosAnimacao.js
+++ src/Scripts/EstudosAnimacao.js
@@ -107,35 +107,35 @@ skills.forEach((item) => {
             var text = document.querySelector('.desc')
        
             if(item.id == 'htmlcontainer'){
-                text.innerHTML = 'Possuo experiência no uso do <strong class="html">HTML</strong> como linguagem de marcação para o desenvolvimento de páginas web. Tenho familiaridade com suas estruturas fundamentais, assim como compreensão do Document Object Model (DOM).<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Possuo experiência no uso do <strong class="html">HTML</strong> como linguagem de marcação para o desenvolvimento de páginas web. Tenho familiaridade com suas estruturas fundamentais, assim como compreensão do Document Object Model (DOM).<strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             
             if(item.id == 'csscontainer'){
-                text.innerHTML = 'Tenho experiência na utilização do <strong class="css">CSS</strong> como linguagem de estilização para páginas web. Possuo conhecimento em estilizar elementos CSS, construir estruturas de design eficazes e criar animações para aprimorar a experiência do usuário.<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Tenho experiência na utilização do <strong class="css">CSS</strong> como linguagem de estilização para páginas web. Possuo conhecimento em estilizar elementos CSS, construir estruturas de design eficazes e criar animações para aprimorar a experiência do usuário.<strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             
             if(item.id == 'jscontainer'){
-                text.innerHTML = 'Desenvolvi habilidades em <strong class="js">JavaScript</strong>, utilizando-o para criar animações envolventes, manipular dinamicamente o Document Object Model (DOM) e construir funcionalidades interativas que agregam valor à experiência do usuário em páginas web.<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Desenvolvi habilidades em <strong class="js">JavaScript</strong>, utilizando-o para criar animações envolventes, manipular dinamicamente o Document Object Model (DOM) e construir funcionalidades interativas que agregam valor à experiência do usuário em páginas web.<strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             
             if(item.id == 'jquerycontainer'){
-                text.innerHTML = 'Aprofundei meu conhecimento ao aprender a utilizar o <strong class="jquery">Jquery</strong>, aprimorando minhas habilidades em efetuar requisições AJAX de forma mais eficiente. Além disso, adquiri expertise na construção de animações envolventes e na configuração facilitada do Document Object Model (DOM), graças a este framework. <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Aprofundei meu conhecimento ao aprender a utilizar o <strong class="jquery">Jquery</strong>, aprimorando minhas habilidades em efetuar requisições AJAX de forma mais eficiente. Além disso, adquiri expertise na construção de animações envolventes e na configuração facilitada do Document Object Model (DOM), graças a este framework. <strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             
             if(item.id == 'phpcontainer'){
-                text.innerHTML = 'Adquiri proficiência em <strong class="php">PHP</strong>, o que me possibilitou desenvolver páginas HTML dinâmicas e estabelecer conexões eficientes com o servidor. Agora, posso criar conteúdo interativo, manipular dados dinamicamente e aprimorar a experiência do usuário em minhas aplicações web <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Adquiri proficiência em <strong class="php">PHP</strong>, o que me possibilitou desenvolver páginas HTML dinâmicas e estabelecer conexões eficientes com o servidor. Agora, posso criar conteúdo interativo, manipular dados dinamicamente e aprimorar a experiência do usuário em minhas aplicações web <strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             
             if(item.id == 'sqlcontainer'){
-                text.innerHTML = 'Adquiri conhecimentos em <strong class="sql">SQL</strong>, o que me habilitou a armazenar dados dos usuários em aplicações em que eu desempenhava um papel mais focado no back-end. Agora, posso gerenciar eficientemente informações essenciais, proporcionando uma base sólida para o desenvolvimento de aplicações robustas <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Adquiri conhecimentos em <strong class="sql">SQL</strong>, o que me habilitou a armazenar dados dos usuários em aplicações em que eu desempenhava um papel mais focado no back-end. Agora, posso gerenciar eficientemente informações essenciais, proporcionando uma base sólida para o desenvolvimento de aplicações robustas <strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             
             if(item.id == 'sasscontainer'){
-                text.innerHTML = 'Atualmente, estou dedicando tempo aos estudos de <strong class="sass">SASS</strong>, utilizando esta linguagem de extensão do CSS no desenvolvimento de sites. Busco aprofundar meu conhecimento nesta poderosa ferramenta, explorando suas funcionalidades para otimizar a criação de estilos e aprimorar a qualidade dos meus projetos web. <strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Atualmente, estou dedicando tempo aos estudos de <strong class="sass">SASS</strong>, utilizando esta linguagem de extensão do CSS no desenvolvimento de sites. Busco aprofundar meu conhecimento nesta poderosa ferramenta, explorando suas funcionalidades para otimizar a criação de estilos e aprimorar a qualidade dos meus projetos web. <strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             
             if(item.id == 'bootscontainer'){
-                text.innerHTML = 'Aprofundei meus conhecimentos em desenvolvimento web ao aprender <strong class="bootstrap">Bootstrap</strong>. Esta experiência enriqueceu minha capacidade de estruturar elementos HTML e aplicar estilos CSS de maneira mais eficiente, utilizando o framework. Além disso, adquiri habilidades para incorporar facilmente funcionalidades adicionais às páginas, tornando o processo de construção mais ágil e eficaz.<strong class="mousehoveraviso"> (Passe o mouse em cima para ver as descrições)</strong>'
+                text.innerHTML = 'Aprofundei meus conhecimentos em desenvolvimento web ao aprender <strong class="bootstrap">Bootstrap</strong>. Esta experiência enriqueceu minha capacidade de estruturar elementos HTML e aplicar estilos CSS de maneira mais eficiente, utilizando o framework. Além disso, adquiri habilidades para incorporar facilmente funcionalidades adicionais às páginas, tornando o processo de construção mais ágil e eficaz.<strong class="mousehoveraviso"> (Passe o mouse em cima ou clique para ver as descrições)</strong>'
             }
             text.style.animation = ' text-focus-in 0.2s steps(60) 1 running'
         }
@@ -146,7 +146,7 @@ skills.forEach((item) => {
             var bg = item.querySelector('.before');
             bg.classList.remove('beforeactive');
             var text = document.querySelector('.desc')
-            text.innerHTML = 'As habilidades que adquiri enquanto estudava.<strong class="mousehoveraviso">(Passe o mouse em cima para ver as descrições)</strong>'
+            text.innerHTML = 'As habilidades que adquiri enquanto estudava.<strong class="mousehoveraviso">(Passe o mouse em cima ou clique para ver as descrições)</strong>'
             text.style.animation = ' text-focus-inB 0.2s steps(60) 1 running'
         }
     });
